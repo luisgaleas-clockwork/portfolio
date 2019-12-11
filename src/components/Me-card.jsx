5042738633082717ad9d335eb9f3439bd7741c31
Luis Galeas <luis.galeasjr@gmail.com>
2019-12-10 21:05:24 -0500

    added accesibility

--- src/components/Me-card.jsx
+++ src/components/Me-card.jsx
@@ -4,7 +4,7 @@ const Me = () => {
     return (
         <div className="me-card">
             <div className="me-img-container animated pulse infinite slower">
-                <img src="https://i.imgur.com/74cTAWp.jpg" />
+                <img src="https://i.imgur.com/74cTAWp.jpg" alt="picture of me"/>
             </div>
             <div className="me-text-container animated fadeIn slow">
                 <h1>Luis Galeas</h1>
--- src/components/Utility/Item.jsx
+++ src/components/Utility/Item.jsx
@@ -6,7 +6,7 @@ const ProjectItems = (props) => {
         <div className="project__container">
             <div className="project_item">
                 <div className="project__image_container">
-                    <img src={work.image} />
+                    <img src={work.image} alt={work.info}/>
                     <div className="project__text_container">
                         <h3>{work.name}</h3>
                         <div className="link__buttons">
